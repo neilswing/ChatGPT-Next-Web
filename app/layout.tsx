f81377111aa1251437bd3f60ae86b37bdf849e0a
kalokohanofficial@gmail.com <kalokohanofficial@gmail.com>
2023-09-26 16:42:35 +0800

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,13 +43,14 @@ export default function RootLayout({
           crossOrigin="anonymous"
         ></script>
         <ins
-          class="adsbygoogle"
-          style="display:block"
+          className="adsbygoogle"
+          style={{ display: "block" }}
           data-ad-client="ca-pub-4592814778191453"
           data-ad-slot="7643469181"
           data-ad-format="auto"
           data-full-width-responsive="true"
         ></ins>
+
         <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
       </body>
     </html>
